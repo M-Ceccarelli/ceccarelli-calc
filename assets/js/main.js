aaafce3ee4a25ec69ba868ab2fb212dda81e4463
M-Ceccarelli <mc-inf@outlook.com>
2025-05-14 10:50:14 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ jQuery(document).ready(function() {
   jQuery("#fat-full").on('keyup change',function(){
     let fase1 = jQuery("#fat-full").val()*0.1;
     let fase2 = jQuery("#fat-full").val()*0.9;
-    fase1 = fase1.toFixed(1);
-    fase2 = fase2.toFixed(1);
+    fase1 = fase1.toFixed(2);
+    fase2 = fase2.toFixed(2);
     jQuery("#fat-fase-1").val(fase1);
     jQuery("#fat-fase-2").val(fase2);
   })
@@ -12,8 +12,8 @@ jQuery(document).ready(function() {
   jQuery("#stamin-full").on('keyup change',function(){
     let fase1 = jQuery("#stamin-full").val()*0.2;
     let fase2 = jQuery("#stamin-full").val()*0.8;
-    fase1 = fase1.toFixed(1);
-    fase2 = fase2.toFixed(1);
+    fase1 = fase1.toFixed(2);
+    fase2 = fase2.toFixed(2);
     jQuery("#stamin-fase-1").val(fase1);
     jQuery("#stamin-fase-2").val(fase2);
   })
